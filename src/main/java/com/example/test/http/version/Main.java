8b5c8d4bb1ea16997bed349c3ad9e8b9a793937e
xerx593 <alexlutz79@gmail.com>
2021-11-25 16:54:44 +0100

    added okhttp, re-format

--- src/main/java/com/example/test/http/version/Main.java
+++ src/main/java/com/example/test/http/version/Main.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
 
-    @GetMapping("/foo")
-    public String foo() {
-        return "bar";
-    }
+  @GetMapping("/foo")
+  public String foo() {
+    return "bar";
+  }
 }
