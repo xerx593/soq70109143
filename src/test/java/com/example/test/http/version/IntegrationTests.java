204ca0cc2aa24ebbb6556f1bf00ec9e6579c5f9e
xerx593 <alexlutz79@gmail.com>
2021-11-25 19:17:53 +0100

    upgrade to http2

--- src/test/java/com/example/test/http/version/IntegrationTests.java
+++ src/test/java/com/example/test/http/version/IntegrationTests.java
@@ -2,10 +2,10 @@ package com.example.test.http.version;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,71 +15,32 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class IntegrationTests {
 
-  @LocalServerPort
-  private int port;
+    @LocalServerPort
+    private int port;
 
-  @Test
-  public void contextLoads() {
-    assertTrue(port > 0 && port <= 2 * Short.MAX_VALUE);
-  }
-
-  @Test
-  public void testJavaDotNet() throws IOException, InterruptedException {
-    // Given:
-    String uri = getUriString("/foo");
-    java.net.http.HttpClient client = java.net.http.HttpClient.newBuilder().build();
-    java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder().uri(URI.create(uri)).build();
-
-    // When:
-    java.net.http.HttpResponse<String> response = client.send(request,
-        java.net.http.HttpResponse.BodyHandlers.ofString());
-
-    // Then:
-    assertNotNull(response);
-    assertEquals(java.net.http.HttpClient.Version.HTTP_1_1, response.version());
-  }
-
-  @Test
-  public void testApache() throws IOException {
-    // Given:
-    String uri = getUriString("/foo");
-    org.apache.http.client.HttpClient client = org.apache.http.impl.client.HttpClientBuilder.create().build();
-
-		// Given:
-    org.apache.http.HttpResponse response = client.execute(new org.apache.http.client.methods.HttpGet(uri));
-
-    // Then:
-    assertNotNull(response);
-    assertNotNull(response.getStatusLine());
-
-    org.apache.http.ProtocolVersion protocolV = response.getStatusLine().getProtocolVersion();
-
-    assertNotNull(protocolV);
-
-    assertEquals("HTTP", protocolV.getProtocol());
-    assertEquals(1, protocolV.getMajor());
-    assertEquals(1, protocolV.getMinor());
-  }
-
-  @Test
-  public void testOkHttp() throws IOException {
-		// Given:
-    okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
-    String uriString = getUriString("/foo");
-
-		// When:
-    okhttp3.Request request = new okhttp3.Request.Builder().url(uriString).build();
-    try (okhttp3.Response response = client.newCall(request).execute()) {
-      
-      // Then:
-      assertEquals(okhttp3.Protocol.HTTP_1_1, response.protocol());
-    } catch (RuntimeException reexc) {
-      fail(reexc);
+    @Test
+    public void contextLoads() {
+        assertTrue(port > 0 && port <= 2 * Short.MAX_VALUE);
     }
-  }
 
-  private String getUriString(String path) {
-    return String.format("http://localhost:%d/%s", port, path);
-  }
+    @Test
+    public void testJavaDotNet() throws IOException, InterruptedException {
+        // Given:
+        String uri = getUriString("foo");
+        java.net.http.HttpClient client = java.net.http.HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+        java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder().uri(URI.create(uri)).build();
+
+        // When:
+        java.net.http.HttpResponse<String> response = client.send(request,
+                java.net.http.HttpResponse.BodyHandlers.ofString());
+
+        // Then:
+        assertNotNull(response);
+        assertEquals(java.net.http.HttpClient.Version.HTTP_2, response.version());
+    }
+
+    private String getUriString(String path) {
+        return String.format("http://localhost:%d/%s", port, path);
+    }
 
 }
